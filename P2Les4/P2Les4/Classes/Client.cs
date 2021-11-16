1a40c2e94d3fe379e7a3368bd4ae797d334e82aa
K4ppach1no <tgheeringa@gmail.com>
2021-11-16 15:20:34 +0100

    Add project files.

---
+++ P2Les4/P2Les4/Classes/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace P2Les4.Classes
+{
+    class Client
+    {
+        static string Socket(string message)
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.137.1"); //Voer hier het IP adres van je computer in!
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000); //Gebruik een andere poort als dat nodig is
+            string 
+            return sentMessage;
+        }
+    }
+}
